dba6296dac63f201d37eda91782262af75c84535
Pavao Piršljin <pavaopirsljin@gmail.com>
2018-06-20 15:44:16 +0200

    Add User and post Model. Install Slug package

---
+++ app/Models/Post.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Models;
+
+//use van klase je za servis
+use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
+
+class Post extends Model
+{
+    //koristi trait Sluggable
+    use Sluggable;
+     /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['title', 'content', 'user_id'];
+    
+    /**
+    * save new post 
+    *
+    * @param array $data
+    * @return object Post -- da ne moramo 2 zahtjeva bazi slati, odmah ga imamo
+    *
+    */
+    public function savePost($data = array())
+    {
+        return $this->create($data);
+    }
+    
+    /**
+    * update post 
+    *
+    * @param array $data
+    * @return void
+    *
+    */
+    
+    public function updatePost($data)
+    {
+        $this->update($data);
+    }
+    
+    public function sluggable()
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
+    }
+     /**
+     * return user relationship
+     *
+     * 
+     */
+    public function user()
+    {   //model user komunicira s bazom
+        return $this->belongsTo('App\Models\User');
+    }
+}
---
+++ app/Models/User.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Cartalyst\Sentinel\Users\EloquentUser;
+
+class User extends EloquentUser
+{
+     /**
+     * Return the posts relationship
+     *
+     * 
+     */
+    public function posts()
+    {
+        return $this->hasMany('App\Models\Post');
+    }
+    
+    
+}
